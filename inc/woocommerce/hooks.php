ac13fd7950a20ff56ecef28c922126f06c8fab15
Charles Harvey <harvey.charles@gmail.com>
2019-06-14 17:40:54 +0100

    asdf

--- inc/woocommerce/hooks.php
+++ inc/woocommerce/hooks.php
@@ -83,7 +83,7 @@ add_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_show_product_
 add_action( 'woocommerce_after_shop_loop_item_title', 'shop_isle_outofstock_notify_on_archives', 10 );
 
 
-add_filter( 'woocommerce_format_price_range', 'chilly_price_html_new', 100, 2 );
+add_filter( 'woocommerce_format_price_range', 'chilly_price_html_new', 100, 3 );
 function chilly_price_html_new( $price, $from, $to ){
     return  sprintf( _x( 'Starting From %1$s', 'Price range: from-to', 'woocommerce' ), is_numeric( $from ) ? wc_price( $from ) : $from );
 }
