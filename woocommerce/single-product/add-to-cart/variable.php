6a138f34a5b8b63685d28426f06a3a0967d1ebf1
Charles Harvey <harvey.charles@gmail.com>
2019-08-25 22:27:18 +0200

    vasd

--- woocommerce/single-product/add-to-cart/variable.php
+++ woocommerce/single-product/add-to-cart/variable.php
@@ -54,15 +54,15 @@ do_action( 'woocommerce_before_add_to_cart_form' ); ?>
                     <?php
                     // // CHARLES EDIT
                     // // non professional users can only see boite-de-50-sachets and 100-grammes
-                    // if ($user_is_professional == false) {
-                    //     if ($attribute_name ==  'pa_poids') {
-                    //         foreach ($options as $key => $option) {
-                    //             if ($option != 'boite-de-20-sachets-de-thes' && $option != 'the-en-vrac-100g'  ) {
-                    //                 unset($options[$key]); 
-                    //             }
-                    //         }
-                    //     }
-                    // }
+                    if ($user_is_professional == false) {
+                        if ($attribute_name ==  'pa_poids') {
+                            foreach ($options as $key => $option) {
+                                if ($option != 'boite-de-15-sachets-de-thes' && $option != 'the-en-vrac-100g'  ) {
+                                    unset($options[$key]); 
+                                }
+                            }
+                        }
+                    }
                     ?>
 
 					<tr>
