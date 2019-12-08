e7d2885baffcc35311e1cc69d8b5507dbb62dec9
Melissa Rissel <rissel.melissa@gmail.com>
2019-12-08 14:37:48 +0100

    no idea

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -232,7 +232,7 @@
 					$('.navbar-custom .navbar-nav  li, .navbar-custom li.dropdown  ul  li').removeClass('open');
 					var delay = 0;
 					var setTimeoutConst;
-					$('.navbar-custom .navbar-nav  li, .navbar-custom li  ul  li').click(
+					$('.navbar-custom .navbar-nav  li, .navbar-custom li  ul  li').on('click',
 						function () {
 							var $this = $(this);
 							setTimeoutConst = setTimeout(
